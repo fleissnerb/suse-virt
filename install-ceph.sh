b5cd4da0620d871a215ce8289e8864b5abecf69c
fleissnerb <36540779+fleissnerb@users.noreply.github.com>
2026-03-12 13:22:56 -0400

    Delete install-ceph.sh

--- install-ceph.sh
+++
@@ -1,3 +0,0 @@
-helm repo add rook-release https://charts.rook.io/release
-helm install --create-namespace --namespace rook-ceph rook-ceph rook-release/rook-ceph -f https://raw.githubusercontent.com/fleissnerb/suse-virt/refs/heads/main/rook-ceph.yaml
-helm install --namespace rook-ceph rook-ceph-cluster --set operatorNamespace=rook-ceph rook-release/rook-ceph-cluster -f https://raw.githubusercontent.com/fleissnerb/suse-virt/refs/heads/main/rook-ceph-cluster.yaml
